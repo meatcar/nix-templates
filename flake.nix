372894d59390c25df41214219714d109278345f3
Denys Pavlov <me@denys.me>
2021-10-18 16:29:53 -0400

    fix: set a defaultTemplate properly

--- flake.nix
+++ flake.nix
@@ -7,8 +7,8 @@
         path = ./direnv-tmux-project;
         description = "A project structure that uses direnv and tmux";
       };
-
-      default = self.templates.direnv-tmux-project;
     };
+
+    defaultTemplate = self.templates.direnv-tmux-project;
   };
 }
