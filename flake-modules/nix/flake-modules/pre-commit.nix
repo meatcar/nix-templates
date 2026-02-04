787df538c70a6ac10c1748fe32510d87a579bd6e
Denys Pavlov <github@denys.me>
2026-02-04 11:57:55 -0500

    feat: add more treefmt formatters/checkers

--- flake-modules/nix/flake-modules/pre-commit.nix
+++ flake-modules/nix/flake-modules/pre-commit.nix
@@ -11,7 +11,15 @@
     };
     formatter = config.treefmt.build.wrapper;
     treefmt.config.programs = {
+      ## Nix
+      # format nix files
       nixfmt.enable = true;
+      # remove unused nix variables (skip with `# deadnix: skip`)
+      deadnix.enable = true;
+      # highlight antipaterns in nix code
+      statix.enable = true;
+      ## Everything else
+      prettier.enable = true;
     };
     pre-commit.check.enable = true;
     pre-commit.settings = {
